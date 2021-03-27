65d648fdcd6942cb544352274a53ad8e0ae5b220
Amith Mohanan <amithm7@gmail.com>
2021-03-27 19:02:49 +0530

    feat: Add context menu for page and links

--- background.js
+++ background.js
@@ -1,3 +1,28 @@
+function openInMpv (url) {
+    chrome.tabs.update({ 'url': 'ytdl://' + url });
+}
+
+
 chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.update({'url':'ytdl://' + tab.url});
+    openInMpv(tab.url);
+});
+
+chrome.contextMenus.create({
+    id: "open-page",
+    title: "Open page with mpv",
+    contexts: ["page"]
+});
+
+chrome.contextMenus.create({
+    id: "open-link",
+    title: "Open link with mpv",
+    contexts: ["link"]
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId == "open-page") {
+        openInMpv(tab.url);
+    } else if (info.menuItemId == "open-link") {
+        openInMpv(info.linkUrl);
+    }
 });
