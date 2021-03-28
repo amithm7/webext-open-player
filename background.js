7c06c43bfd450c0e6611d34448ec1e285334697f
Amith Mohanan <amithm7@gmail.com>
2021-03-29 03:10:05 +0530

    feat: Options page to select players

--- background.js
+++ background.js
@@ -1,47 +1,66 @@
 function openInMpv (url) {
     chrome.tabs.update({ 'url': 'ytdl://' + url });
+    console.log("open mpv");
 }
 
 function openInVLC (url) {
     chrome.tabs.update({ 'url': 'vlc://' + url });
+    console.log("open vlc");
 }
 
-chrome.browserAction.onClicked.addListener((tab) => {
-    openInMpv(tab.url);
-});
+chrome.storage.sync.get({
+    defaultPlayer: 'mpv',
+    players: {
+        mpv: true,
+        vlc: true
+    }
+}, function (items) {
+    chrome.browserAction.onClicked.addListener((tab) => {
+        if(items.defaultPlayer == 'mpv') {
+            openInMpv(tab.url);
+        } else if (items.defaultPlayer == 'vlc') {
+            openInVLC(tab.url);
+        }
+    });
+    if(items.players.mpv) {
+        chrome.contextMenus.create({
+            id: "open-page-mpv",
+            title: "Open page with mpv",
+            contexts: ["page"]
+        });
 
-chrome.contextMenus.create({
-    id: "open-page",
-    title: "Open page with mpv",
-    contexts: ["page"]
-});
+        chrome.contextMenus.create({
+            id: "open-link-mpv",
+            title: "Open link with mpv",
+            contexts: ["link"]
+        });
+        chrome.contextMenus.onClicked.addListener(function (info, tab) {
+            if (info.menuItemId == "open-page-mpv") {
+                openInMpv(tab.url);
+            } else if (info.menuItemId == "open-link-mpv") {
+                openInMpv(info.linkUrl);
+            }
+        });
+    }
+    if(items.players.vlc) {
+        chrome.contextMenus.create({
+            id: "open-page-vlc",
+            title: "Open page with VLC",
+            contexts: ["page"]
+        });
 
-chrome.contextMenus.create({
-    id: "open-link",
-    title: "Open link with mpv",
-    contexts: ["link"]
-});
+        chrome.contextMenus.create({
+            id: "open-link-vlc",
+            title: "Open link with VLC",
+            contexts: ["link"]
+        });
 
-chrome.contextMenus.create({
-    id: "open-page-vlc",
-    title: "Open page with VLC",
-    contexts: ["page"]
-});
-
-chrome.contextMenus.create({
-    id: "open-link-vlc",
-    title: "Open link with VLC",
-    contexts: ["link"]
-});
-
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId == "open-page") {
-        openInMpv(tab.url);
-    } else if (info.menuItemId == "open-link") {
-        openInMpv(info.linkUrl);
-    } else if (info.menuItemId == "open-page-vlc") {
-        openInVLC(tab.url);
-    } else if (info.menuItemId == "open-link-vlc") {
-        openInVLC(info.linkUrl);
+        chrome.contextMenus.onClicked.addListener(function (info, tab) {
+            if (info.menuItemId == "open-page-vlc") {
+                openInVLC(tab.url);
+            } else if (info.menuItemId == "open-link-vlc") {
+                openInVLC(info.linkUrl);
+            }
+        });
     }
 });
---
+++ options.js
@@ -0,0 +1,40 @@
+// Saves options to chrome.storage
+function save_options() {
+    var defaultPlayer = document.getElementById('default').value;
+    var mpvCheck = document.getElementById('mpv').checked;
+    var vlcCheck = document.getElementById('vlc').checked;
+
+    chrome.storage.sync.set({
+        defaultPlayer: defaultPlayer,
+        players: {
+            mpv: mpvCheck,
+            vlc: vlcCheck
+        }
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 3000);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default values are specified here
+    chrome.storage.sync.get({
+        defaultPlayer: 'mpv',
+        players: {
+            mpv: true,
+            vlc: true
+        }
+    }, function (items) {
+        document.getElementById('default').value = items.defaultPlayer;
+        document.getElementById('mpv').checked = items.players.mpv;
+        document.getElementById('vlc').checked = items.players.vlc;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
