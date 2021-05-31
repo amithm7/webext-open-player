217a35cd2c6e9f360509d040c02d50e0cc2137d6
Amith Mohanan <amithm7@gmail.com>
2021-05-31 10:42:09 +0530

    fix: Multiple event listeners being created
    Context menus had multiple event listeners, casuing duplicate empty
    url's being opened or other players opened. Observed on firefox.

--- src/background.js
+++ src/background.js
@@ -1,11 +1,14 @@
-function openInMpv (url) {
-    chrome.tabs.update({ 'url': 'ytdl://' + url });
-    console.log("open mpv");
-}
-
-function openInVLC (url) {
-    chrome.tabs.update({ 'url': 'vlc://' + url });
-    console.log("open vlc");
+function openPlayer (player, url) {
+    switch (player) {
+        case 'mpv':
+            chrome.tabs.update({ 'url': 'ytdl://' + url });
+            console.log("open mpv");
+            break;
+        case 'vlc':
+            chrome.tabs.update({ 'url': 'vlc://' + url });
+            console.log("open vlc");
+            break;
+    }
 }
 
 function createMenu (player) {
@@ -25,20 +28,6 @@ function createMenu (player) {
         id: player + "-video",
         title: player + " - Open video",
         contexts: ["video"]
-    });
-}
-
-function listenMenu (player) {
-    chrome.contextMenus.onClicked.addListener(function (info, tab) {
-        let url = "";
-        if (info.menuItemId == player + "-page") {
-            url = tab.url;
-        } else if (info.menuItemId == player + "-link") {
-            url = info.linkUrl;
-        } else if (info.menuItemId == player + "-video") {
-            url = info.srcUrl;
-        }
-        player == "mpv" ? openInMpv(url) : openInVLC(url);
     });
 }
 
@@ -69,18 +58,20 @@ chrome.storage.local.get({
     }
 }, function (items) {
     chrome.browserAction.onClicked.addListener((tab) => {
-        if (items.defaultPlayer == 'mpv') {
-            openInMpv(tab.url);
-        } else if (items.defaultPlayer == 'vlc') {
-            openInVLC(tab.url);
-        }
+        openPlayer(items.defaultPlayer, tab.url);
     });
-    if (items.players.mpv) {
-        listenMenu('mpv');
-    }
-    if (items.players.vlc) {
-        listenMenu('vlc');
-    }
+    chrome.contextMenus.onClicked.addListener(function (info, tab) {
+        let player = info.menuItemId.split('-')[0];
+        let url = "";
+        if (info.menuItemId == player + "-page") {
+            url = tab.url;
+        } else if (info.menuItemId == player + "-link") {
+            url = info.linkUrl;
+        } else if (info.menuItemId == player + "-video") {
+            url = info.srcUrl;
+        }
+        openPlayer(player, url);
+    });
 });
 
 // When options are changed
